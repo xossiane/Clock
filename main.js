eb531cb0b6dd9d7499a729a5774909368bb4c00f
xossiane <anejosi.lima@gmail.com>
2022-08-09 18:57:30 -0300

    Inserção das funções dos ponteiros

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate(){
   const now = new Date ();
@@ -15,6 +17,14 @@ function setDate(){
   const secondsDegrees = ((seconds / 60) * 360 + 90);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`
   console.log(seconds)
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360 + 90);
+  minHand.style.transform = `rotate(${minutesDegrees}deg`
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 60) * 360 + 90);
+  hourHand.style.transform = `rotate(${hoursDegrees}deg`
 }
 setInterval(setDate, 1000);
 
