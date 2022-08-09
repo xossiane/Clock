9ac454f3b87bc1d16c1c67562164b1989d0d21cd
xossiane <anejosi.lima@gmail.com>
2022-08-09 19:04:30 -0300

    Relogio finalizado

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
-  <h1> Hello </h1>
+  <h1> Hello, this is our clock</h1>
   </div>
 `
 const secondHand = document.querySelector('.second-hand');
@@ -16,15 +16,16 @@ function setDate(){
   //how do we turn this into degrees?
   const secondsDegrees = ((seconds / 60) * 360 + 90);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-  console.log(seconds)
+  
 
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360 + 90);
   minHand.style.transform = `rotate(${minutesDegrees}deg`
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 60) * 360 + 90);
+  const hoursDegrees = ((hours / 60) * 360 + 180);
   hourHand.style.transform = `rotate(${hoursDegrees}deg`
+  console.log(hours)
 }
 setInterval(setDate, 1000);
 
